73929d0aa8917ee204dbbd384bb4bdc65980fd38
conor2019 <71699812+conor2019@users.noreply.github.com>
2022-12-19 19:34:21 +0000

    Add files via upload

--- IMD_CA2/js/myFuncs.js
+++ IMD_CA2/js/myFuncs.js
@@ -2,6 +2,8 @@ var modal = document.getElementById("myModal");
 
 var btn = document.getElementById("contactBtn");
 
+var darkBtn = document.getElementById("dark_mode");
+
 btn.onclick = function() {
     modal.style.display = "block";
 }
@@ -10,4 +12,9 @@ window.onclick = function(event) {
     if (event.target == modal) {
         modal.style.display = "none";
     }
+}
+
+function darkFunc() {
+    var element = document.body;
+    element.classList.toggle("dark-mode");
 }
